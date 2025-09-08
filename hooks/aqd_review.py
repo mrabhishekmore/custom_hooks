acbcc660d68031e56f2f5d6153858ea32ee7cdad
Abhishek Sunil More <abhishekmore157@gmail.com>
2025-09-08 13:38:59 +0530

    Create aqd_review.py

---
+++ hooks/aqd_review.py
@@ -0,0 +1,18 @@
+import keyboard
+import time
+
+
+def main():
+    time.sleep(3)
+
+    # Press Alt + Win + Q
+    keyboard.press_and_release('alt+windows+q')
+
+    time.sleep(1)
+
+    keyboard.write("review workspace")
+    keyboard.press_and_release('enter')
+
+
+if __name__ == "__main__":
+    main()
